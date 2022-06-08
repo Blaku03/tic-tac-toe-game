bd943aee5665118a8e60a36c78a19b7ff53f206a
Bartek <the.bainow03@gmail.com>
2022-06-08 13:57:01 +0200

    final build of the game

--- game.py
+++ game.py
@@ -30,6 +30,37 @@ def game():
         " ",
     ]
 
+    round = 0
+
+    what_player = player1_side
+
+    while round < 9 and winning_con(values_board):
+
+        display_board(values_board)
+
+        cell = valid_board_number(values_board)
+        round += 1
+
+        clear()
+
+        if what_player == player1_side:
+            values_board[cell] = player1_side
+            what_player = player2_side
+            winner = player1_side
+        else:
+            values_board[cell] = player2_side
+            what_player = player1_side
+            winner = player2_side
+
+    # end of the game message
+    clear()
+
+    if winning_con(values_board) == False:
+        print(f"{winner} have won the game!")
+
+    else:
+        print("It's a draw!")
+
 
 # display numbers corresponding to cell in play grid
 def introduction():
@@ -46,7 +77,7 @@ def introduction():
         exit()
 
     clear()
-    print("Great grid is represented by numbers like this:")
+    print("Great!\nGrid is represented by numbers as follows: ")
 
     display_board(introduction_board)
 
@@ -54,15 +85,54 @@ def introduction():
     clear()
 
 
-def winning_con():
-    pass
+def winning_con(board):
+
+    nobody_won = True
+
+    # row check
+    for i in range(1, 8, 3):
+
+        if board[i] == board[i + 1] == board[i + 2]:
+            if board[i] == "X" or board[i] == "O":
+                nobody_won = False
+
+    #  column check
+    for i in range(1, 4):
+
+        if board[i] == board[i + 3] == board[i + 6]:
+            if board[i] == "X" or board[i] == "O":
+                nobody_won = False
+
+    # diagonal check
+    if board[1] == board[5] == board[9] or board[3] == board[5] == board[7]:
+        if board[5] == "X" or board[5] == "O":
+            nobody_won = False
+
+    return nobody_won
 
 
-def valid_board_number():
-    pass
+def valid_board_number(board):
+
+    try:
+        cell = int(input("Choose a cell from 1 to 9: "))
+
+        while cell not in range(1, 10):
+
+            cell = int(input("Please eneter a valid number: "))
+
+    except:
+        cell = int(input("Please enter a number in base 10: "))
+
+        while cell not in range(1, 10):
+
+            cell = int(input("Please eneter a valid number: "))
+
+    while board[cell] != " ":
+        cell = int(input("This cell is already selected choose diffrent tile: "))
+
+    return cell
 
 
-# TODO funconial board
 def display_board(board):
 
     print(board[1] + "|" + board[2] + "|" + board[3])
@@ -83,43 +153,5 @@ def choose_side():
     return player_input
 
 
-# game
 introduction()
-
-# player1_side = choose_side()
-# round = 0
-# player1_turn = True
-# player_pick = None
-
-# values_board = [
-#     " ",
-#     " ",
-#     " ",
-#     " ",
-#     " ",
-#     " ",
-#     " ",
-#     " ",
-#     " ",
-#     " ",
-# ]
-
-# while round < 9:
-
-#     if player1_turn:
-
-#         player_pick = int(input("Choose number from 1 to 9: "))
-#         clear()
-#         values_board[player_pick] = player1_side
-#         player1_turn = False
-
-#     else:
-
-#         player_pick = int(input("Choose number from 1 to 9: "))
-#         clear()
-#         values_board[player_pick] = player2_side
-#         player1_turn = True
-
-#     display_board(values_board)
-
-#     round += 1
+game()
